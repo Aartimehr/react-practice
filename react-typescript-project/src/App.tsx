8e4403a25bd69bf2cb5863e2503cb54b67f3992a
Aartimehr <mehra11aarti@gmail.com>
2026-03-13 01:29:01 +0530

    added a table for http requests

--- react-typescript-project/src/App.tsx
+++ react-typescript-project/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
- export default function App() {
+  function App() {
   return(
     <>
     <h1>React TypeScript Practice</h1>
@@ -7,7 +7,36 @@ import { useState } from 'react'
        <h2>Welcome to my website</h2>
        <button>open a page</button>
     </div>
+    <section>
+    <table>
+      <tr>
+        <th>HTTP Requests</th>
+        <th>Description</th>
+      </tr>
+      <tr>
+        <td>GET</td>
+        <td>Retrieve data from a server</td>
+      </tr>
+      <tr>
+        <td>POST</td>
+        <td>Submit data to a server</td>
+      </tr>
+      <tr>
+        <td>PUT/Update</td>
+        <td>Update existing data on a server</td>
+      </tr>
+      <tr>
+        <td>DELETE</td>
+        <td>Delete data from a server</td>
+      </tr>
+      
+    </table>
+  </section>
     </>
   )
+
 }
+
+export default App;
+
 
