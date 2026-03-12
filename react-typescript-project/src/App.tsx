e0636dbe9c2ad49efe2043275a63ba141c40a5fb
Aartimehr <mehra11aarti@gmail.com>
2026-03-13 01:13:18 +0530

    typescript code

---
+++ react-typescript-project/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+ export default function App() {
+  return(
+    <>
+    <h1>React TypeScript Practice</h1>
+    <div>
+       <h2>Welcome to my website</h2>
+       <button>open a page</button>
+    </div>
+    </>
+  )
+}
+
